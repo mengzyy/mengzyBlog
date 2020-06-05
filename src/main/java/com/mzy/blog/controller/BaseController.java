1404f9b0df74d948b483a5857ca17e3df0121552
18306299232 <18306299232@163.com>
2020-06-05 22:50:10 +0800

    初始化mengzyblog项目

---
+++ src/main/java/com/mzy/blog/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.mzy.blog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * controller基类
+ * @program: mengzyBlog
+ * @author: mengzy 18306299232@163.com
+ * @create: 2020-06-05 22:41
+ **/
+@Slf4j
+public class BaseController {
+}
